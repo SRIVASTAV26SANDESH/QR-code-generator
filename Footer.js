6ae691126f53c924ec048553a5ac118f71e14e74
Sandesh Srivastav <142063424+SRIVASTAV26SANDESH@users.noreply.github.com>
2024-09-18 01:15:56 +0530

    successfully added

---
+++ Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+   return (
+      <footer className="attribution">
+         <p>developed by <a href="https://github.com/SRIVASTAV26SANDESH" target="_blank" rel="noreferrer">Sandesh Srivastav</a></p>
+      </footer>
+   );
+}
+
+export default Footer;
